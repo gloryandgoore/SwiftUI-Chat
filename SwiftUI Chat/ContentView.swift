d7a287183a41d1c1f071ff4181e798e6fb6aba24
Caseyann <goore_44@hotmail.ca>
2022-08-29 11:55:49 -0400

    Initial Commit

---
+++ SwiftUI Chat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Chat
+//
+//  Created by Caseyann Goore on 2022-08-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Chat/SwiftUI_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChatApp.swift
+//  SwiftUI Chat
+//
+//  Created by Caseyann Goore on 2022-08-29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
